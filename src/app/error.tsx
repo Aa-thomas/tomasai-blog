84c714de3e559d94db0a1aa8b8c4d765f8ca09e4
Aaron Thomas <aarthomas01@gmail.com>
2023-07-30 14:09:00 -0400

    Add loading and Error pages

---
+++ src/app/error.tsx
@@ -0,0 +1,9 @@
+const Error = () => {
+	return (
+		<div>
+			<p>Oops! Seems that there was an error</p>
+		</div>
+	);
+};
+
+export default Error;
---
+++ src/app/loading.tsx
@@ -0,0 +1,5 @@
+import { ClipLoader } from 'react-spinners';
+
+const Loading = () => <ClipLoader loading={true} color="#36d7b7" />;
+
+export default Loading;
