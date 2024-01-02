1e213e79e6b42c76cb7458aeeca17e940674c4f1
Aaron Thomas <aartomas01@gmail.com>
2024-01-01 20:36:56 -0500

    add more FC types

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,3 +1,5 @@
-export const Loading = () => (
-	<span className="flex items-center justify-center loading loading-dots loading-lg"></span>
+import { FC } from 'react';
+
+export const Loading: FC = () => (
+	<span className='flex items-center justify-center loading loading-dots loading-lg'></span>
 );
