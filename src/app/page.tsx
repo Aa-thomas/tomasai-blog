fb1d29e03cd1617fcf69dfe4e11b69605874e9cd
Aaron Thomas <aarthomas01@gmail.com>
2024-02-25 13:10:05 -0500

    Add content to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image';
 
 export default function Home() {
 	return (
-		<main className="flex flex-col items-center justify-between min-h-screen"></main>
+		<main className='flex flex-col items-center justify-between min-h-screen'>
+			This is the main page
+		</main>
 	);
 }
